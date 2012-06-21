7a085b676e9658170ee624919f8536eb9f547f5a
John McKerrell <john@mckerrell.net>
2012-06-21 10:28:28 +0100

    Updating to remove push-button code and add LEDs

--- WhereDialPCBTest.ino
+++ WhereDialPCBTest.ino
@@ -23,8 +23,7 @@ const int stepsPerRevolution = 2048;  // change this to fit the number of steps 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 7,5,6,4);            
 const int buttonPin1 = 2, buttonPin2 = 3;
-const int ledPin1 = 0, ledPin2 = 1;
-int ledState1 = LOW, ledState2 = LOW;
+const int ledPin1 = 0, ledPin2 = 1, ledPin3 = 2, ledPin4 = 3;
 
 void setup() {
   // set the speed at 60 rpm:
@@ -35,25 +34,40 @@ void setup() {
   pinMode(buttonPin2, INPUT);
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(ledPin1,HIGH);
+  digitalWrite(ledPin2,HIGH);
+  digitalWrite(ledPin3,HIGH);
+  digitalWrite(ledPin4,HIGH);
+  delay(1000);
+  digitalWrite(ledPin4,LOW);
+  delay(1000);
+  digitalWrite(ledPin1,LOW);
+  digitalWrite(ledPin2,LOW);
+  digitalWrite(ledPin3,LOW);
+  delay(500);
+  digitalWrite(ledPin1,HIGH);
+  digitalWrite(ledPin2,HIGH);
+  digitalWrite(ledPin3,HIGH);
+  delay(1000);
+  digitalWrite(ledPin3,LOW);
+  delay(1000);
+  digitalWrite(ledPin2,LOW);
+  delay(1000);
+  digitalWrite(ledPin1,LOW);
+  
   // step one revolution  in one direction:
 //   Serial.println("clockwise");
-  myStepper.step(50);//stepsPerRevolution);
+  myStepper.step(200);//stepsPerRevolution);
   delay(1000);
   
 //  Serial.println("counterclockwise");
-  myStepper.step(50);//-stepsPerRevolution);
+  myStepper.step(-100);//-stepsPerRevolution);
   delay(1000); 
   
-  if ( digitalRead(buttonPin1) == LOW ) {
-    ledState1 = ledState1 == HIGH ? LOW : HIGH;
-  }
-  if ( digitalRead(buttonPin2) == LOW ) {
-    ledState2 = ledState2 == HIGH ? LOW : HIGH;
-  }
-  digitalWrite(ledPin1, ledState1);
-  digitalWrite(ledPin2, ledState2);
 }
 
